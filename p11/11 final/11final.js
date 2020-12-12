928e814509e70cdb4c829cfd96b68a475d44afc6
test user <test@mail.ru>
2020-12-12 18:49:49 +0200

    added final 11

--- p11/11 final/11final.js
+++ p11/11 final/11final.js
@@ -4,7 +4,7 @@ var max = diapason.split(',')[1];
 
  */
 
-var high = 100;
+/* var high = 100;
 var low = 0;
 var result = null;
 var currentValue;
@@ -14,16 +14,42 @@ alert(
 );
 
 do {
-  currentValue = high / 2;
+  currentValue = parseInt((high + low) / 2);
   result = prompt(
     "Ваше число " +
       currentValue +
       ". Якщо ми вгадали то напишіть Guessed, якщо ваше число менше - напишіть lower, якщо більше - higer"
   );
   if (result == "lower") {
-    high = high - currentValue / 2;
+    
+    high = parseInt(high / 2);
   }
   if (result == "higer") {
-    high = high + currentValue / 2;
+    low = currentValue;
+    high = parseInt((high + currentValue) / 2);
+  }
+} while (result != "Guessed");
+ */
+
+var high = 100;
+var low = 0;
+var result = null;
+var currentValue;
+alert(
+  "Вітаємо! Загадайте число від 1 до 100. Наш алгоритм спробує його відгадати"
+);
+
+do {
+  currentValue = parseInt((high + low) / 2);
+  result = prompt(
+    "Ваше число " +
+      currentValue +
+      ". Якщо ми вгадали то напишіть Guessed, якщо ваше число менше - напишіть lower, якщо більше - higer"
+  );
+  if (result == "lower") {
+    high = currentValue;
+  }
+  if (result == "higer") {
+    low = currentValue;
   }
 } while (result != "Guessed");
